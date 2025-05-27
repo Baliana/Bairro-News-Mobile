ea5f64a1dad54c1866a26e89b0a5ca9246d9540d
FERNANDO BALIANA DE MIRANDA <24122781@dsn127m1247443.cfp127aluno.local>
2025-05-27 17:27:40 -0300

    parte visual das telas estao feitas

--- app/src/main/java/br/senai/sp/jandira/bairronews/screen/TelaHome.kt
+++ app/src/main/java/br/senai/sp/jandira/bairronews/screen/TelaHome.kt
@@ -1,20 +1,17 @@
 package br.senai.sp.jandira.bairronews.screen
-
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Send
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,10 +19,10 @@ import androidx.navigation.NavHostController
 
 @Composable
 fun TelaHome(navController: NavHostController?) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+        // Topo
         Card(
             modifier = Modifier
                 .padding(top = 65.dp)
@@ -35,7 +32,7 @@ fun TelaHome(navController: NavHostController?) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(16.dp),
+                    .padding(horizontal = 16.dp, vertical = 12.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
@@ -45,28 +42,44 @@ fun TelaHome(navController: NavHostController?) {
                         contentDescription = "Mais opções",
                         tint = Color.Black
                     )
-
+                    Spacer(modifier = Modifier.width(8.dp))
                     Text(
                         fontSize = 15.sp,
-                        text ="Bairro News",
-                        fontWeight = FontWeight.Bold,
+                        text = "Bairro News",
                         color = Color.Red
                     )
                 }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(Icons.Default.Search, contentDescription = "Pesquisar")
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Icon(Icons.Default.Send, contentDescription = "Enviar")
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Icon(Icons.Default.Person, contentDescription = "Usuário")
+                }
             }
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(Icons.Default.Search, contentDescription = "Pesquisar")
-                Spacer(modifier = Modifier.width(4.dp))
-                Icon(Icons.Default.Send, contentDescription = "Enviar")
-                Spacer(modifier = Modifier.width(4.dp))
-                Icon(Icons.Default.Person, contentDescription = "Usuário")
-                Spacer(modifier = Modifier.width(4.dp))
-            }
-
         }
+
+        Spacer(modifier = Modifier.height(45.dp))
+
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(horizontal = 16.dp)
+        ){}
+
+        Spacer(modifier = Modifier.height(45.dp))
+
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(horizontal = 16.dp)
+        ){}
     }
 }
-
 @Preview(showSystemUi = true)
 @Composable
 private fun TelaHomePreview() {
